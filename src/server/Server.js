11860282bdf8bec61df1f4e0fe1e907bd9fce42c
Miyu Togo <togomiyu@gmail.com>
2022-02-15 08:32:11 +0800

    trial 1

--- src/server/Server.js
+++ src/server/Server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
+        redirectUri: 'https://miyutogo.github.io/Miyusic_App/',
         clientId: '9a14d7c8301240a3a0f999068d197353',
         clientSecret:'2c9681f88c3b4b08b0b46b9617472645',
     })
@@ -35,7 +35,7 @@ app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
 
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
+        redirectUri: 'https://miyutogo.github.io/Miyusic_App/',
         clientId: '9a14d7c8301240a3a0f999068d197353',
         clientSecret:'2c9681f88c3b4b08b0b46b9617472645',
         refreshToken,
